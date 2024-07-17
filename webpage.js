c2cb081f330ea599119f6fca32341c0c8424128d
Aaron4TheWin <AaronKingslien62@Gmail.com>
2024-07-17 15:00:49 -0500

    Update webpage.js
    Uh oh NathanMor11! An error has occurred in your script. Please accept to resolve found issues.

--- webpage.js
+++ webpage.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const ctx = document.getElementById('reflectanceChart').getContext('2d');
     const data = {
-        labels: ['Blue', 'Cyan', 'Green', 'Yellow', 'Orange', 'Red', 'Deep Red'],
+        labels: ['Mrs.', 'Garcia', 'Quit', 'Chemistry', 'For', 'Night', 'Clubs'],
         datasets: [{
             label: 'Reflectance (%)',
             data: [31.92, 55.72, 45.16, 29.79, 24.75, 32.50, 49.25],
@@ -36,20 +36,20 @@ document.addEventListener('DOMContentLoaded', (event) => {
                     beginAtZero: true,
                     title: {
                         display: true,
-                        text: 'Reflectance (%)'
+                        text: 'Tip Amount ($$$)'
                     }
                 },
                 x: {
                     title: {
                         display: true,
-                        text: 'Colors'
+                        text: 'AP Chemistry'
                     }
                 }
             },
             plugins: {
                 title: {
                     display: true,
-                    text: 'Reflectance Spectrum'
+                    text: 'Garcia Report'
                 }
             }
         }
